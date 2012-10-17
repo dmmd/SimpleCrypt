3c083427f7811b23d2ed2d18e291e089da1b6e48
Don Mennerich <domenn@gmail.com>
2012-10-17 16:33:04 -0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Donald
+ *
+ * Created on October 17, 2012, 4:30 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
